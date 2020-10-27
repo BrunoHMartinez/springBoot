67c310e06629063efe86930b43727f30753bcc46
BrunoHMartinez <bruno.h_m@hotmail.com>
2020-10-27 17:25:55 -0300

    Category

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -8,35 +8,46 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
-	
-	//Exemplo de injeção de dependência
+
+	// Exemplo de injeção de dependência
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
-		User u1 = new User(null, "Bruno","bruno@gmail.com" , "9999-9999", "123456789");
-		User u2 = new User(null, "Hadassa","hadassa@gmail.com" , "9999-88888", "987654321");
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
 		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+
+		User u1 = new User(null, "Bruno", "bruno@gmail.com", "9999-9999", "123456789");
+		User u2 = new User(null, "Hadassa", "hadassa@gmail.com", "9999-88888", "987654321");
+
 		Order o1 = new Order(null, Instant.parse("2020-10-21T19:53:07Z"), u1, OrderStatus.PAID);
 		Order o2 = new Order(null, Instant.parse("2020-10-21T03:53:07Z"), u1, OrderStatus.WAITING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2020-10-21T15:53:07Z"), u1, OrderStatus.DELIVERED);
- 
-		
-		userRepository.saveAll(Arrays.asList(u1,u2));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
-	}}
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+	}
+}
