f12e578527c4959efba8c8a2bedd4afcf185238c
BrunoHMartinez <bruno.h_m@hotmail.com>
2020-10-30 14:54:24 -0300

    OrderItem pt2

---
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.OrderItem;
+import com.educandoweb.course.entities.PK.OrderItemPK;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> { 
+}
